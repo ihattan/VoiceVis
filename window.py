4e1169b068c9c24d7b11d97dada08e0411174a15
Ian Hattan <ian.j.hattan@gmail.com>
2020-07-08 01:30:58 -0700

    Adjusted initialization settings after PyQt5 research. Screen size fix.

--- window.py
+++ window.py
@@ -1,37 +1,47 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QIcon, QPainter, QBrush, QPen
-from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtCore import Qt, QRect, QCoreApplication
 
-class App(QMainWindow):
+class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = 'Voice Vis 0.1'
-        self.left = 150
-        self.top = 40
-        self.width = 1000
-        self.height = 1000
+
+        self.screen = QCoreApplication.instance().primaryScreen().availableGeometry()
+        self.x_adjust = int(self.screen.width() * 0.1)
+        self.y_adjust = int(self.screen.height() * 0.1)
+        self.width = self.screen.width() - self.x_adjust
+        self.height = self.screen.height() - self.y_adjust
+
         self.initUI()
         self.paintEvent(None)
 
     def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowTitle('Voice Vis 0.1')
+
+        #self.setWindowFlags(Qt.CustomizeWindowHint)
+        #self.setWindowFlags(Qt.FramelessWindowHint)
+
+        self.setGeometry(int(self.x_adjust / 2), int(self.y_adjust / 2), self.width, self.height)
+        self.setFixedSize(self.width, self.height)
+
         self.show()
 
     def paintEvent(self, event):
-        center = (int(self.width/2), int(self.height/2))
+        centerX = int(self.width / 2)
+        centerY = int(self.height / 2)
+
         number_of_points = 12
         span_angle = int(360 / number_of_points)
         offset = int(0.5 * span_angle)
         points_on_circle = [(p * span_angle) + offset for p in range(number_of_points)]
 
-        outer_radius = 400
-        outer_circle_frame = QRect(center[0] - outer_radius, center[1] - outer_radius, outer_radius*2, outer_radius*2)
+        outer_radius = int(min(self.width, self.height) / 3)
+        outer_circle_frame = QRect(centerX - outer_radius, centerY - outer_radius, outer_radius*2, outer_radius*2)
 
-        inner_radius = 200
-        inner_circle_frame = QRect(center[0] - inner_radius, center[1] - inner_radius, inner_radius*2, inner_radius*2)
+        inner_radius = int(outer_radius / 2)
+        inner_circle_frame = QRect(centerX - inner_radius, centerY - inner_radius, inner_radius*2, inner_radius*2)
 
         painter = QPainter()
         painter.begin(self)
