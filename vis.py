3eb30b6fe71936cb3630fbe1b31ac35d3d6a60a2
Ian Hattan <ian.j.hattan@gmail.com>
2020-07-08 17:54:13 -0700

    QHBoxLayout added, fileButton added for wave file input

--- vis.py
+++ vis.py
@@ -1,10 +1,13 @@
-from PyQt5.QtCore import QRect
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon, QPainter, QBrush, QPen, QColor
+from PyQt5.QtCore import Qt, QRect
 
-class Vis():
+class VisWidget(QWidget):
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self):
+        super().__init__()
+
         self.numSlices = 12
         self.spanAngle = int(360 / self.numSlices)
 
@@ -17,7 +20,9 @@ class Vis():
         self.frstFrame = QRect()
         self.scndFrame = QRect()
         self.thrdFrame = QRect()
-        self.calcFrames()
+        #self.calcFrames()
+
+        self.paintEvent(None)
 
 
     def calcSlicePoints(self):
@@ -31,13 +36,41 @@ class Vis():
 
         # first is largest, 1/3 the height of the given area
         # rest of the radii are 2/3 and 1/3 the size of the first
-        frstRadius = int(self.height / 3)
+        frstRadius = int(min(self.width(), self.height()) / 3)
         scndRadius = int(frstRadius * 2 / 3)
         thrdRadius = int(frstRadius * 1 / 3)
 
         # center of the visualization is offset to the left
-        visCenterX = visCenterY = int(self.height / 2)
+        visCenterX = int(self.width() / 2)
+        visCenterY = int(self.height() / 2)
 
         self.frstFrame = QRect(visCenterX - frstRadius, visCenterY - frstRadius, frstRadius*2, frstRadius*2)
         self.scndFrame = QRect(visCenterX - scndRadius, visCenterY - scndRadius, scndRadius*2, scndRadius*2)
         self.thrdFrame = QRect(visCenterX - thrdRadius, visCenterY - thrdRadius, thrdRadius*2, thrdRadius*2)
+
+    def paintEvent(self, event):
+        self.calcFrames()
+
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        self.drawVis(painter)
+        painter.end()
+
+    def drawVis(self, painter):
+        angleFrac = 16 # angles measured in 16ths when drawing
+        painter.setPen(Qt.white)
+
+        for p in self.slicePoints:
+            painter.setBrush(QColor(148, 141, 179))
+            painter.drawPie(self.frstFrame, p * angleFrac, self.spanAngle * angleFrac)
+            painter.setBrush(QColor(96, 88, 133))
+            painter.drawPie(self.scndFrame, p * angleFrac, self.spanAngle * angleFrac)
+            painter.setBrush(QColor(67, 59, 103))
+            painter.drawPie(self.thrdFrame, p * angleFrac, self.spanAngle * angleFrac)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = VisWidget()
+    ex.show()
+    sys.exit(app.exec_())
--- window.py
+++ window.py
@@ -1,6 +1,7 @@
 import sys
-from vis import Vis
-from PyQt5.QtWidgets import QApplication, QWidget
+from vis import VisWidget
+from read_wave import WaveIO
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QHBoxLayout, QVBoxLayout, QFileDialog
 from PyQt5.QtGui import QIcon, QPainter, QBrush, QPen, QColor
 from PyQt5.QtCore import Qt, QCoreApplication, QRect #, QPoint, QSize
 
@@ -14,40 +15,35 @@ class App(QWidget):
         self.yAdjust = int(self.screen.height() * 0.1)
         self.width = self.screen.width() - self.xAdjust
         self.height = self.screen.height() - self.yAdjust
-        self.vis = Vis(self.width, self.height)
 
         self.initUI()
 
     def initUI(self):
+        fileButton = QPushButton('Input File')
+        fileButton.setMaximumWidth(int(self.width / 4))
+        fileButton.clicked.connect(lambda : self.fileInput())
+
+        mainHBox = QHBoxLayout()
+        mainHBox.addWidget(VisWidget())
+        mainHBox.addWidget(fileButton)
+        self.setLayout(mainHBox)
+
         self.setWindowTitle('Voice Vis 0.1')
 
         #self.setWindowFlags(Qt.CustomizeWindowHint)
         #self.setWindowFlags(Qt.FramelessWindowHint)
 
         self.setGeometry(int(self.xAdjust / 2), int(self.yAdjust / 2), self.width, self.height)
-        self.setFixedSize(self.width, self.height)
+        #self.setFixedSize(self.width, self.height)
 
-        self.paintEvent(None)
         self.show()
 
-    def paintEvent(self, event):
-        painter = QPainter()
-        painter.begin(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-        self.drawVis(painter)
-        painter.end()
-
-    def drawVis(self, painter):
-        angleFrac = 16 # angles measured in 16ths when drawing
-        painter.setPen(Qt.white)
-
-        for p in self.vis.slicePoints:
-            painter.setBrush(QColor(148, 141, 179))
-            painter.drawPie(self.vis.frstFrame, p * angleFrac, self.vis.spanAngle * angleFrac)
-            painter.setBrush(QColor(96, 88, 133))
-            painter.drawPie(self.vis.scndFrame, p * angleFrac, self.vis.spanAngle * angleFrac)
-            painter.setBrush(QColor(67, 59, 103))
-            painter.drawPie(self.vis.thrdFrame, p * angleFrac, self.vis.spanAngle * angleFrac)
+    def fileInput(self):
+        fin = QFileDialog()
+        fname, _ = fin.getOpenFileName(self, filter='Wave File (*.wav)')
+        if fname is not None:
+            waveObj = WaveIO()
+            waveObj.read_wave(fname)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
